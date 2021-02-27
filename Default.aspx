b28d8c0e72a1bed2e01adedd7a9690e38de903c8
Ahmed Thaajwer <ahamedthaajwer5528@gmail.com>
2021-02-27 13:09:54 +0530

    first commit

---
+++ Default.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FileProjectUpload._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <script src="Scripts/jquery-3.4.1.js" type="text/javascript"></script>
+    <script language ="javascript" type="text/javascript">
+
+        $(function () {
+            var scntDiv = $('#FileUploaders');
+            var i = $('#FileUploaders p').size() + 1;
+
+            $('#addUploader').live('click', function () {
+                $('<p><input type="file" id="FileUploader' + 1 + '" name="FileUploader' + 1 + '" /></label> <a href="#" id="removeUploader">Remove</a></p>').appendTo(scntDiv);
+               
+                i++;
+                return false;
+            });
+            $('#removeUploader').live('click', function () {
+                if (i > 2) {
+                    $(this).parent('p').remove();
+                    i--;
+                }
+                return false;
+            });
+        });
+    </script>
+
+<h3>Upload Your Files</h3>
+    <div style="padding:10px; border:1px solid blue">
+    <div id="FileUploaders">
+        <p>
+            <input type="file" id="FileUploader1" name="FileUploader1" />
+        </p>
+    </div>
+
+        <a href="#" id="addUploader">Add Another</a>
+   <br />
+    <asp:Button ID="btnUploadAll" runat="server" Text="Upload File(s)" OnClick="btnUploadAll_Click" />
+    </div>
+    <br />
+    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" CellPadding="10">
+        <Columns>
+            <asp:BoundField HeaderText="File name" DataField="FileName" />
+            <asp:BoundField HeaderText="File size" DataField="FileSize" />
+            <asp:TemplateField>
+                <ItemTemplate>
+                    <asp:LinkButton ID="IbtnDownload" runat="server" CommandName="Download" CommandArgument='<%#Eval("FileID") %>'>
+                        Download</asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+
+    </asp:GridView>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FileProjectUpload
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.Form.Enctype = "multipart/form-data";
+            if (!IsPostBack)
+            {
+                PopulateUploadFiles();
+            }
+
+        }
+        private void PopulateUploadFiles()
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                GridView1.DataSource = dc.UploadedFIles.ToList();
+                GridView1.DataBind();
+            }
+        }
+        protected void btnUploadAll_Click(object sender, EventArgs e)
+        {
+
+            HttpFileCollection filesColl = Request.Files;
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                foreach (string uploader in filesColl)
+                {
+                    HttpPostedFile file = filesColl[uploader];
+                    if (file.ContentLength > 0)
+                    {
+
+                        BinaryReader br = new BinaryReader(file.InputStream);
+                        byte[] buffer = br.ReadBytes(file.ContentLength);
+
+                        dc.UploadedFIles.Add(new UploadedFIle
+                        {
+
+                            FileID = 0,
+                            FileName = file.FileName,
+                            ContentType = file.ContentType,
+                            FileExtention = Path.GetExtension(file.FileName),
+                            FileSize = file.ContentLength,
+                            FileContetnt = buffer
+
+                        });
+
+
+
+                    }
+                }
+                dc.SaveChanges();
+            }
+            PopulateUploadFiles();
+        }
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Download")
+            {
+                int fileID = Convert.ToInt32(e.CommandArgument.ToString());
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    var v = dc.UploadedFIles.Where(a => a.FileID.Equals(fileID)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Response.ContentType = v.ContentType;
+                        Response.AddHeader("content-disposition", "attachment; filename" + v.FileName);
+                        Response.BinaryWrite(v.FileContetnt);
+                        Response.Flush();
+                        Response.End();
+
+                    }
+                }
+            }
+        }
+    }
+}
